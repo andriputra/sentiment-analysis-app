217ddc043337d793da3be19836f5bc062e720f1a
agusandriputra <andriputra.job@gmail.com>
2024-06-16 06:32:43 +0700

    done result

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -3,46 +3,52 @@
 <head>
     <title>Results</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/style/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container mt-4">
-        <h3 class="mb-3">Sentiment Analysis Results</h3>
-        <div class="table-responsive">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Opsi Dataset</th>
-                        <th colspan="3">Tweet Positif</th>
-                        <th colspan="3">Tweet Negatif</th>
-                        <th>Akurasi</th>
-                    </tr>
-                    <tr>
-                        <th></th>
-                        <th>Presisi</th>
-                        <th>Recall</th>
-                        <th>F1-Score</th>
-                        <th>Presisi</th>
-                        <th>Recall</th>
-                        <th>F1-Score</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Overall</td>
-                        <td>{{ isset($results['overall']['pos']['precision']) ? number_format($results['overall']['pos']['precision'], 2) : '-' }}</td>
-                        <td>{{ isset($results['overall']['pos']['recall']) ? number_format($results['overall']['pos']['recall'], 2) : '-' }}</td>
-                        <td>{{ isset($results['overall']['pos']['f1']) ? number_format($results['overall']['pos']['f1'], 2) : '-' }}</td>
-                        <td>{{ isset($results['overall']['neg']['precision']) ? number_format($results['overall']['neg']['precision'], 2) : '-' }}</td>
-                        <td>{{ isset($results['overall']['neg']['recall']) ? number_format($results['overall']['neg']['recall'], 2) : '-' }}</td>
-                        <td>{{ isset($results['overall']['neg']['f1']) ? number_format($results['overall']['neg']['f1'], 2) : '-' }}</td>
-                        <td>{{ number_format($results['overall']['accuracy'], 4) }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <div class="box-result">
+            <h3 class="mb-3 text-center">Sentiment Analysis Results</h3>
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Opsi Dataset</th>
+                            <th colspan="3">Tweet Positif</th>
+                            <th colspan="3">Tweet Negatif</th>
+                            <th>Akurasi</th>
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <th>Presisi</th>
+                            <th>Recall</th>
+                            <th>F1-Score</th>
+                            <th>Presisi</th>
+                            <th>Recall</th>
+                            <th>F1-Score</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Overall</td>
+                            <td>{{ isset($results['overall']['pos']['precision']) ? number_format($results['overall']['pos']['precision'], 2) : '-' }}</td>
+                            <td>{{ isset($results['overall']['pos']['recall']) ? number_format($results['overall']['pos']['recall'], 2) : '-' }}</td>
+                            <td>{{ isset($results['overall']['pos']['f1']) ? number_format($results['overall']['pos']['f1'], 2) : '-' }}</td>
+                            <td>{{ isset($results['overall']['neg']['precision']) ? number_format($results['overall']['neg']['precision'], 2) : '-' }}</td>
+                            <td>{{ isset($results['overall']['neg']['recall']) ? number_format($results['overall']['neg']['recall'], 2) : '-' }}</td>
+                            <td>{{ isset($results['overall']['neg']['f1']) ? number_format($results['overall']['neg']['f1'], 2) : '-' }}</td>
+                            <td>{{ number_format($results['overall']['accuracy'], 4) }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="button-action text-end">
+                <a href="/upload" class="btn btn-sm btn-warning">Back To Upload</a>
+            </div>
+        </div>        
     </div>
 </body>
 </html>
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -3,16 +3,36 @@
 <head>
     <title>Upload CSV</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/style/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
-    <div class="container">
-        <form action="/upload" method="post" enctype="multipart/form-data">
-            @csrf
-            <input type="file" name="csv_file" accept=".csv" class="form-control">
-            <button type="submit" class="btn btn-primary">Upload</button>
-        </form>
+    <div class="container-sm">
+        <div class="box">
+            <div class="title-app text-center mb-4">
+                <h4>Data Model Sentiment</h4>
+                <h5>Pemilihan Umum Presiden dan Wakil Presiden Republik Indonesia</h5>
+                <h5>Periode 2024-2029</h5>
+            </div>
+            <hr>
+            <form id="upload-form" action="/upload" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="input-group">
+                    <input type="file" name="csv_file" accept=".csv" class="form-control">
+                    <button type="submit" class="btn btn-primary">Process Data</button>
+                </div>
+            </form>
+            <div class="loader mt-3" id="loader"></div>
+        </div>
     </div>
+    <script>
+        $(document).ready(function() {
+            $('#upload-form').submit(function() {
+                $('#loader').show();
+            });
+        });
+    </script>
 </body>
 </html>
